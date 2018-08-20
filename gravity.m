206127f1d05a2b4fefba9de1f4463f731287dcbd
yanxuanm <joshuaktf@gmail.com>
2018-08-20 11:26:22 -0400

    add need files

--- gravity.m
+++
@@ -1,9 +0,0 @@
-% This function calculates the gravitational force applied on the bar. 
-
-function [gravity] = gravitational(qVector, mass)
-    gravity = zeros(size(qVector,1),1);
-    gravity((end/2+2):3:end) = 9.8;
-    
-    %convert acceleration to force using mass matrix 
-    gravity((end/2+1):end) = mass*gravity((end/2+1):end);
-end
